46fd08dbf109fb56f0d0ee6696ebb62cdda8f4ea
Tej Charfi <charfi@usc.edu>
2020-08-18 15:24:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+def reverse_each_word_with_each(string)
+ 
+  original_arr = string.split(" ")
+  reversed_arr = []
+  original_arr.each do|word|
+    reversed_arr << word.reverse
+  end
+  reversed_arr.join(" ")
+end
+
+
+#with collect 
+def reverse_each_word(string) 
+  
+  original_arr =string.split(" ")
+  reversed_arr = []
+  
+  original_arr.collect do |word|
+      reversed_arr << word.reverse  
+  end
+    reversed_arr.join(" ")
+  
+end
