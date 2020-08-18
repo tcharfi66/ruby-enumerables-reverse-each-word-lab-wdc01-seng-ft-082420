445803484903a1f0df3120f355d03133c8652cb1
Tej Charfi <charfi@usc.edu>
2020-08-18 15:19:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,14 +9,3 @@ def reverse_each_word(string)
 end
 
 
-#with collect 
-def reverse_each_word(string) do
-  
-  original_arr =string.split(" ")
-  reversed_arr = []
-  original_arr.collect do |word|
-      reversed_arr << word.reverse  
-  end
-  reversed_arr.join(" ")
-  
-end
